37c1f09407b7dca85369f2d0ca66352fce329c82
Souchy <robyn.g@hotmail.fr>
2025-07-18 13:45:22 -0400

    First commit

---
+++ Souchy.Net/io/Config.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace Souchy.Net.io;
+
+public class Config
+{
+    public static string BaseDirectory { get; set; } = "";
+
+    [JsonIgnore]
+    private string _savePath = "";
+
+    public static T Load<T>(string name = "") where T : Config, new()
+    {
+        if (name == "")
+            name = typeof(T).Name;
+        if (!name.Contains('.'))
+            name += ".json";
+
+        string path = name;
+        if (!string.IsNullOrEmpty(BaseDirectory))
+            path = Path.Combine(BaseDirectory, name);
+
+        if (!File.Exists(path))
+        {
+            var t = new T();
+            t._savePath = path;
+            t.Save();
+            return t;
+        }
+
+        string json = File.ReadAllText(path);
+        T config = Json.Deserialize<T>(json) ?? throw new Exception($"Failed to deserialize config from {path}");
+        config._savePath = path;
+        return config;
+    }
+
+    public void Save()
+    {
+        string json = Json.Serialize(this);
+        File.WriteAllText(_savePath, json);
+    }
+
+}
---
+++ Souchy.Net/io/Json.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Souchy.Net.io;
+
+public static class Json
+{
+
+    public static JsonSerializerSettings Settings { get; set; } = new()
+    {
+        Formatting = Formatting.Indented,
+        TypeNameHandling = TypeNameHandling.Auto,
+        NullValueHandling = NullValueHandling.Ignore,
+        ObjectCreationHandling = ObjectCreationHandling.Replace,
+        MissingMemberHandling = MissingMemberHandling.Ignore,
+    };
+
+    public static string Serialize(object obj)
+    {
+        return JsonConvert.SerializeObject(obj, Settings);
+    }
+
+    public static T? Deserialize<T>(string json)
+    {
+        return JsonConvert.DeserializeObject<T>(json, Settings);
+    }
+
+}
