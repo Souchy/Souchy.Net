918c20e77ed1c9991bc73d7a54edb26ed0bebd36
Souchy <robyn.g@hotmail.fr>
2025-07-19 15:13:07 -0400

    DirectoryUtil

---
+++ Souchy.Net/io/DirectoryUtil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Souchy.Net.io;
+
+public static class DirectoryUtil
+{
+    public static void MoveDirectoryRec(string source, string target, bool overwrite = true)
+    {
+        var sourcePath = source.TrimEnd('\\', ' ');
+        var targetPath = target.TrimEnd('\\', ' ');
+        var files = Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                             .GroupBy(Path.GetDirectoryName);
+        foreach (var folder in files)
+        {
+            var targetFolder = folder.Key.Replace(sourcePath, targetPath);
+            Directory.CreateDirectory(targetFolder);
+            foreach (var file in folder)
+            {
+                var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                bool exists = File.Exists(targetFile);
+
+                if (overwrite && exists)
+                {
+                    File.Delete(targetFile);
+                }
+                if (overwrite || !exists)
+                {
+                    File.Move(file, targetFile);
+                }
+
+            }
+        }
+        Directory.Delete(source, true);
+    }
+
+    public static void CopyDirectory(string source, string target, bool overwrite = true)
+    {
+        var sourcePath = source.TrimEnd('\\', ' ');
+        var targetPath = target.TrimEnd('\\', ' ');
+        var files = Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                             .GroupBy(Path.GetDirectoryName);
+        foreach (var folder in files)
+        {
+            var targetFolder = folder.Key.Replace(sourcePath, targetPath);
+            Directory.CreateDirectory(targetFolder);
+            foreach (var file in folder)
+            {
+                var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                File.Copy(file, targetFile, overwrite);
+            }
+        }
+    }
+
+}
