6c456229795ea70458c3bbaee58ef5eb3a8a61c4
Souchy <robyn.g@hotmail.fr>
2025-07-20 12:23:31 -0400

    Create Naming.cs

---
+++ Souchy.Net/Naming.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Souchy.Net;
+
+public class Naming
+{
+    public static string ToCamelCase(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+        if (input.Length == 1)
+            return input.ToLowerInvariant();
+        return char.ToLowerInvariant(input[0]) + input.Substring(1);
+    }
+
+    public static string ToPascalCase(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+        if (input.Length == 1)
+            return input.ToUpperInvariant();
+        return char.ToUpperInvariant(input[0]) + input.Substring(1);
+    }
+
+    public static string ToSnakeCase(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+        var sb = new StringBuilder();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char c = input[i];
+            bool isDigit = char.IsDigit(c);
+            if ((char.IsUpper(c) || isDigit) && i > 0 && char.IsLower(input[i - 1])) //(char.IsLower(input[i - 1]) || char.IsDigit(input[i - 1])))
+            {
+                sb.Append('_');
+            }
+            sb.Append(char.ToLowerInvariant(c));
+        }
+        return sb.ToString();
+    }
+
+    public static string ToKebabCase(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+        var sb = new StringBuilder();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char c = input[i];
+            bool isDigit = char.IsDigit(c);
+            if ((char.IsUpper(c) || isDigit) && i > 0 && char.IsLower(input[i - 1]) ) //(char.IsLower(input[i - 1]) || char.IsDigit(input[i - 1])))
+            {
+                sb.Append('-');
+            }
+            sb.Append(char.ToLowerInvariant(c));
+        }
+        return sb.ToString();
+    }
+
+}
